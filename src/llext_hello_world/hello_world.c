075fbd9692918fb32fa59df780fedb2d969c1eaa
Dennis <45356478+DennisMoschina@users.noreply.github.com>
2025-04-10 18:53:14 +0200

    removed .inc file creation in cmake

--- src/llext_hello_world/hello_world.c
+++ src/llext_hello_world/hello_world.c
@@ -1,8 +1,23 @@
 #include <zephyr/llext/symbol.h>
 #include <zephyr/sys/printk.h>
+#include <zephyr/kernel.h>
 
 void hello_world(void) {
     printk("Hello from dynamically loaded extension!\n");
+    printk("allocating 300 bytes\n");
+
+    // allocate 300 bytes
+    char *buf = alloca(300);
+    if (buf == NULL) {
+        printk("Failed to allocate memory\n");
+        return;
+    }
+    // fill the buffer with 'A's
+    for (int i = 0; i < 300; i++) {
+        buf[i] = 'A';
+    }
+    buf[299] = '\0'; // null-terminate the string
+    printk("Allocated buffer: %s\n", buf);
 }
 
 LL_EXTENSION_SYMBOL(hello_world);
