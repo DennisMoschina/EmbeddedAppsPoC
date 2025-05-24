6654658e42acc95c43ac43bdc2e55c487c20725d
Dennis <45356478+DennisMoschina@users.noreply.github.com>
2025-05-24 21:24:58 +0200

    updated mpconfigport_custom with all possible options

--- src/microPython/mpconfigport_custom.h
+++ src/microPython/mpconfigport_custom.h
@@ -29,29 +29,31 @@
 #include "autoconf.h"
 // Included here to get basic Zephyr environment (macros, etc.)
 #include <zephyr/kernel.h>
+#include <zephyr/drivers/spi.h>
 
 // Usually passed from Makefile
 #ifndef MICROPY_HEAP_SIZE
 #define MICROPY_HEAP_SIZE (16 * 1024)
 #endif
 
+#define MICROPY_MODULE_FROZEN_MPY (0)
+#define MICROPY_MODULE_FROZEN_STR (0)
+#define MICROPY_MODULE_FROZEN (0)
+
+#define MICROPY_ENABLE_SOURCE_LINE  (0)
 #define MICROPY_STACK_CHECK         (1)
 #define MICROPY_ENABLE_GC           (1)
 #define MICROPY_ENABLE_FINALISER    (MICROPY_VFS)
 #define MICROPY_HELPER_REPL         (1)
 #define MICROPY_REPL_AUTO_INDENT    (1)
 #define MICROPY_KBD_EXCEPTION       (1)
-#define MICROPY_CPYTHON_COMPAT      (0)
 #define MICROPY_PY_ASYNC_AWAIT      (0)
-#define MICROPY_PY_ATTRTUPLE        (0)
-#define MICROPY_PY_BUILTINS_ENUMERATE (0)
+#define MICROPY_PY_BUILTINS_BYTES_HEX (0)
 #define MICROPY_PY_BUILTINS_FILTER  (0)
 #define MICROPY_PY_BUILTINS_MIN_MAX (0)
 #define MICROPY_PY_BUILTINS_PROPERTY (0)
 #define MICROPY_PY_BUILTINS_RANGE_ATTRS (0)
 #define MICROPY_PY_BUILTINS_REVERSED (0)
-#define MICROPY_PY_BUILTINS_SET     (0)
-#define MICROPY_PY_BUILTINS_SLICE   (0)
 #define MICROPY_PY_BUILTINS_STR_COUNT (0)
 #define MICROPY_PY_BUILTINS_MEMORYVIEW (0)
 #define MICROPY_PY_BUILTINS_HELP    (0)
@@ -60,13 +62,48 @@
 #define MICROPY_PY_COLLECTIONS      (0)
 #define MICROPY_PY_CMATH            (0)
 #define MICROPY_PY_IO               (0)
+#define MICROPY_PY_MICROPYTHON_MEM_INFO (0)
+#define MICROPY_PY_MACHINE          (0)
+#define MICROPY_PY_MACHINE_INCLUDEFILE "microPython/modmachine.c"
+#define MICROPY_PY_MACHINE_I2C      (0)
+#define MICROPY_PY_MACHINE_SPI      (0)
+#define MICROPY_PY_MACHINE_SPI_MSB (SPI_TRANSFER_MSB)
+#define MICROPY_PY_MACHINE_SPI_LSB (SPI_TRANSFER_LSB)
+#define MICROPY_PY_MACHINE_PIN_MAKE_NEW mp_pin_make_new
+#define MICROPY_PY_MACHINE_UART     (0)
+#define MICROPY_PY_MACHINE_UART_INCLUDEFILE "microPython/machine_uart.c"
+#ifdef CONFIG_WATCHDOG
+#define MICROPY_PY_MACHINE_WDT      (0)
+#define MICROPY_PY_MACHINE_WDT_INCLUDEFILE  "microPython/machine_wdt.c"
+#endif
+#define MICROPY_PY_MACHINE_PWM      (0)
+#define MICROPY_PY_MACHINE_PWM_INCLUDEFILE "microPython/machine_pwm.c"
 #define MICROPY_PY_STRUCT           (0)
+#ifdef CONFIG_NETWORKING
+// If we have networking, we likely want errno comfort
+#define MICROPY_PY_ERRNO            (1)
+#define MICROPY_PY_SOCKET           (1)
+#endif
+#ifdef CONFIG_BT
+#define MICROPY_PY_BLUETOOTH        (0)
+#ifdef CONFIG_BT_CENTRAL
+#define MICROPY_PY_BLUETOOTH_ENABLE_CENTRAL_MODE (0)
+#endif
+#define MICROPY_PY_BLUETOOTH_ENABLE_GATT_CLIENT (0)
+#endif
+#define MICROPY_PY_BINASCII         (0)
+#define MICROPY_PY_HASHLIB          (0)
+#define MICROPY_PY_OS               (0)
+#define MICROPY_PY_TIME             (0)
+#define MICROPY_PY_TIME_TIME_TIME_NS (0)
+#define MICROPY_PY_TIME_INCLUDEFILE "ports/zephyr/modtime.c"
+#define MICROPY_PY_ZEPHYR           (0)
+#define MICROPY_PY_ZSENSOR          (0)
 #define MICROPY_PY_SYS_MODULES      (0)
-#define MICROPY_LONGINT_IMPL (MICROPY_LONGINT_IMPL_LONGLONG)
+#define MICROPY_LONGINT_IMPL        (MICROPY_LONGINT_IMPL_MPZ)
 #define MICROPY_FLOAT_IMPL (MICROPY_FLOAT_IMPL_FLOAT)
 #define MICROPY_PY_BUILTINS_COMPLEX (0)
-
-// #define MICROPY_ENABLE_SCHEDULER    (1)
+#define MICROPY_ENABLE_SCHEDULER    (0)
 #define MICROPY_VFS                 (1)
 #define MICROPY_READER_VFS          (MICROPY_VFS)
 
@@ -82,6 +119,18 @@
 #define MICROPY_COMP_CONST (0)
 #define MICROPY_COMP_DOUBLE_TUPLE_ASSIGN (0)
 
+// When CONFIG_THREAD_CUSTOM_DATA is enabled, MICROPY_PY_THREAD is enabled automatically
+#ifdef CONFIG_THREAD_CUSTOM_DATA
+#define MICROPY_PY_THREAD                   (1)
+#define MICROPY_PY_THREAD_GIL               (1)
+#define MICROPY_PY_THREAD_GIL_VM_DIVISOR    (32)
+#endif
+
+void mp_hal_signal_event(void);
+#define MICROPY_SCHED_HOOK_SCHEDULED mp_hal_signal_event()
+
+// #define MICROPY_PY_SYS_PLATFORM "zephyr"
+
 #ifdef CONFIG_BOARD
 #define MICROPY_HW_BOARD_NAME "zephyr-" CONFIG_BOARD
 #else
@@ -94,10 +143,30 @@
 #define MICROPY_HW_MCU_NAME "unknown-cpu"
 #endif
 
-typedef int mp_int_t; // must be pointer size
-typedef unsigned mp_uint_t; // must be pointer size
+typedef intptr_t mp_int_t; // must be pointer size
+typedef uintptr_t mp_uint_t; // must be pointer size
 typedef long mp_off_t;
 
 #define MP_STATE_PORT MP_STATE_VM
 
-#define MICROPY_EVENT_POLL_HOOK
+// extra built in names to add to the global namespace
+#define MICROPY_PORT_BUILTINS \
+    { MP_ROM_QSTR(MP_QSTR_open), MP_ROM_PTR(&mp_builtin_open_obj) },
+
+#if MICROPY_PY_THREAD
+#define MICROPY_EVENT_POLL_HOOK \
+    do { \
+        extern void mp_handle_pending(bool); \
+        mp_handle_pending(true); \
+        MP_THREAD_GIL_EXIT(); \
+        k_msleep(1); \
+        MP_THREAD_GIL_ENTER(); \
+    } while (0);
+#else
+#define MICROPY_EVENT_POLL_HOOK \
+    do { \
+        extern void mp_handle_pending(bool); \
+        mp_handle_pending(true); \
+        k_msleep(1); \
+    } while (0);
+#endif
